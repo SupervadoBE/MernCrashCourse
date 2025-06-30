9799fa25b78d9956b47c7ee208571c91a1e82fdb
SupervadoBE <35794721+SupervadoBE@users.noreply.github.com>
2025-06-30 19:44:45 +0300

    start HomaPage & fetch data from server

--- frontend/src/pages/HomaPage.jsx
+++ frontend/src/pages/HomaPage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "../components/ProductCard";
 
 const HomaPage = () => {
-  return (
-    <div>HomaPage</div>
-  )
-}
+  const { fetchProducts, products } = useProductStore();
 
-export default HomaPage
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  console.log("products: ", products);
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🛒
+        </Text>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          No product has been created yet. Please create a product to see it
+          here.{"  "}
+          <Link to="/create">
+            <Text
+              as="span"
+              color="blue.500"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomaPage;
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,26 +1,31 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
-    products: [],
-    setProducts: (products) => set({ products }),
+  products: [],
+  setProducts: (products) => set({ products }),
 
-    createProduct: async (newProduct) => {
-        if(!newProduct.name || !newProduct.price || !newProduct.image){
-            return {success:false, message:"Please fill in all fields."}
-        }
-        const res = await fetch("/api/products", {
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(newProduct)
-        })
-        const data = await res.json()
-        set((state) => ({
-            products: [...state.products, data.data]
-        }));
-        return { success:true, message:"Product created successfully" }
-    },
+  createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.price || !newProduct.image) {
+      return { success: false, message: "Please fill in all fields." };
+    }
+    const res = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+    const data = await res.json();
+    set((state) => ({
+      products: [...state.products, data.data],
+    }));
+    return { success: true, message: "Product created successfully" };
+  },
+  fetchProducts: async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    set({ products: data.data });
+  },
 }));
 // This store can be used to manage the product state in your application.
 // You can import this store in your components and use it to get or set products.
